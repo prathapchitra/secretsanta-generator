1465bb33801146a8bd793293073e8ce55fce082d
prathapchitra <107908670+prathapchitra@users.noreply.github.com>
2025-11-19 18:00:21 +0530

    Update Person.java

--- src/main/java/com/notthebest/demo/model/Person.java
+++ src/main/java/com/notthebest/demo/model/Person.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Person {
 
     @Id
-    private String id;
+    private String id;     // MongoDB uses String ID
 
     private String name;
     private String email;
@@ -19,27 +19,5 @@ public class Person {
         this.email = email;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // getters and setters
 }
