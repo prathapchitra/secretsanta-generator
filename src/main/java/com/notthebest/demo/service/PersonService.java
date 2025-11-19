01cf645fad3af6b07aaf0d126a00a37c1223bf05
prathapchitra <107908670+prathapchitra@users.noreply.github.com>
2025-11-19 18:01:25 +0530

    Update PersonService.java

--- src/main/java/com/notthebest/demo/service/PersonService.java
+++ src/main/java/com/notthebest/demo/service/PersonService.java
@@ -1,62 +1,32 @@
 package com.notthebest.demo.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.notthebest.demo.model.Person;
 import com.notthebest.demo.repository.PersonRepo;
+import java.util.List;
 
 @Service
 public class PersonService {
-	
-	@Autowired
-	private PersonRepo repo;
-	
-	public void deletePerson(int id) {
-		Person person = repo.findById(id).get();
-		repo.delete(person);
-	}
-	
-	public void addPerson(Person person) {
-		repo.save(person);
-	}
-	public List<Person> getPeople() {
-		return repo.findAll();
-	}
-	
-	public Map<String, String> generateMatches() {
-		List<Person> list = getPeople();
-		int N = list.size();
-		boolean[] used =  new boolean[N];
-		int oddOneOut = -1;
-		Map<String, String> matches = new HashMap<>();
-		
-		if(N % 2 == 1) {
-			oddOneOut = randomMatch(N);
-			matches.put(list.get(oddOneOut).getName(), "Nobody");
-		}
-		
-		for(int i = 0; i<N; i++) {
-			if(i == oddOneOut) continue;
-			
-			int match = randomMatch(N);
-			while(match == i || used[match] == true) {
-				match = randomMatch(N);
-			}
-			used[match] = true;
 
-			matches.put(list.get(i).getName(), list.get(match).getName());
-		}
-		
-		return matches;
-		
-	}
-	
-	private int randomMatch(int size) {
-		return (int)(Math.random()*size);
-	}
+    private final PersonRepo repo;
+
+    public PersonService(PersonRepo repo) {
+        this.repo = repo;
+    }
+
+    public Person getPerson(String id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public void deletePerson(String id) {
+        repo.deleteById(id);
+    }
+
+    public Person savePerson(Person person) {
+        return repo.save(person);
+    }
+
+    public List<Person> getAllPersons() {
+        return repo.findAll();
+    }
 }
