47b12da3d327fd2ece5523254d4277b2adae78ef
prathapchitra <107908670+prathapchitra@users.noreply.github.com>
2025-11-19 18:00:55 +0530

    Update PersonRepo.java

--- src/main/java/com/notthebest/demo/repository/PersonRepo.java
+++ src/main/java/com/notthebest/demo/repository/PersonRepo.java
@@ -4,5 +4,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.notthebest.demo.model.Person;
 
 public interface PersonRepo extends MongoRepository<Person, String> {
-
 }
